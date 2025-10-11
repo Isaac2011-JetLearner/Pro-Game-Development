0ba583de6674ae3008dacd74f4d8e497d4c1abf4
Isaac2011-JetLearner <oliveredosa@yahoo.com>
2025-10-11 13:03:02 +0100

    updated

--- lesson2/homework.py
+++ lesson2/homework.py
@@ -1,51 +1,65 @@
 import pygame
+import random
 
 pygame.init()
-
 clock = pygame.time.Clock()
 
 screen = pygame.display.set_mode((600,600))
+
+bee = pygame.image.load("images/bee.png")
+bg = pygame.image.load("images/grassbackground.png")
+flower = pygame.image.load("images/flower.png")
+
 running = True
+x = 300
+y = 450
 
-bg = pygame.image.load("images/background.png")
-rocket = pygame.image.load("images/spaceship.png")
+x_1 = random.randint(10,590)
+y_1 = 10
 
-y = 250
-x = 250
 up = False
 left = False
 right = False
+down = False
 
 while running:
     clock.tick(60)
-    screen.blit(bg, (0,0))
-    screen.blit(rocket, (x,y))
+    screen.blit(bg,(0,0))
+    screen.blit(bee,(x,y))
+    screen.blit(flower,(x_1,y_1))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
             pygame.quit()
-        
-        if event.type == pygame.KEYDOWN:
+    
+    if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
                 up = True
-        if event.type == pygame.KEYUP:
+    if event.type == pygame.KEYUP:
             if event.key == pygame.K_UP:
                 up = False
 
-        if event.type == pygame.KEYDOWN:
+    if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
                 left = True
-        if event.type == pygame.KEYUP:
+    if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT:
                 left = False
 
-        if event.type == pygame.KEYDOWN:
+    if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
                 right = True
-
-        if event.type == pygame.KEYUP:
+    if event.type == pygame.KEYUP:
             if event.key == pygame.K_RIGHT:
                 right = False
+
+
+    if event.type == pygame.KEYDOWN:
+         if event.key == pygame.K_DOWN:
+              down = True
+    if event.type == pygame.KEYUP:
+         if event.key == pygame.K_DOWN:
+              down = False
 
     if up:
         y-=4
@@ -56,7 +70,9 @@ while running:
     if right:
         x+=4
 
-    y+=2
-    pygame.display.update()
+    if down:
+         y+=4
+         
+    y_1+=3
 
-    
+    pygame.display.update()
